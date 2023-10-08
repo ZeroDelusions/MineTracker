0456e3a5a44c13a7e912f67eb6315410fcf434e4
ZeroDelusions <121663433+ZeroDelusions@users.noreply.github.com>
2023-10-08 20:45:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const dotenv = require('dotenv')
+const dotenv = require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
 const { Telegraf } = require('telegraf')
